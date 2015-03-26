d8333acd0ed81cd147afc126c5848c64daef99ff
Toilal <toilal.dev@gmail.com>
2015-03-26 12:05:29 +0100

    Fix dropdown

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
       && location.hostname == this.hostname) {
       var $target = $(this.hash);
-      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      $target = $target.length && ($target || $('[name=' + this.hash.slice(1) + ']'));
       if ($target.length) {
         var targetOffset = $target.offset().top;
         $('html,body').animate({scrollTop: targetOffset}, 600);
